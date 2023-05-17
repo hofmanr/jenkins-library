2d27472190e7ca9b3d251590b50689533d4a193d
Rinus Hofman <rhofman14@gmail.com>
2023-05-17 22:27:09 +0200

    auditTools

--- vars/auditTools.groovy
+++ vars/auditTools.groovy
@@ -1,10 +1,8 @@
 def call(Map config) {
-    node {
-        echo "${config.message}"
-        sh '''
-         git version
-         mvn --version
-         echo "M2_HOME = ${M2_HOME}"
-      '''
-    }
+    echo "${config.message}"
+    sh '''
+        git version
+        mvn --version
+        echo "M2_HOME = ${M2_HOME}"
+     '''
 }
